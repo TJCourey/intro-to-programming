2498c430a68f9b67164fcdcd4948d225ab281c3b
TJ Courey <tjcourey84@gmail.com>
2022-12-05 15:22:22 -0500

    inital project

---
+++ HelloWorldSolution/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HelloWorld;
+
+Console.WriteLine("Hello, World!");
+Console.Write("How Many minutes :");
+string? minutes = Console.ReadLine(); 
+if (minutes is not null)
+{
+    DateUtils utils = new DateUtils();
+    int mins = int.Parse(minutes);
+    DateTime timeAtEndOfBreak =utils.TakeABreak(mins);
+    Console.WriteLine($"ok be back at {timeAtEndOfBreak:T}");
+}
+else
+{
+    Console.WriteLine("Enter number");
+
+}
